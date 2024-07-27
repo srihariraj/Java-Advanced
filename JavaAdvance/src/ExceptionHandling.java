a1211e2942fa1e5f129a4759f467bff00842ac46
srihariraj <srihariatom@gmail.com>
2024-07-27 15:24:34 +0530

    basic arithmetic exception

---
+++ JavaAdvance/src/ExceptionHandling.java
@@ -0,0 +1,23 @@
+
+public class ExceptionHandling {
+
+	public static void main(String[] args) {
+
+		int a = 100;
+		int b = 0;
+		int ans ;
+		
+		try {
+			ans = a/b;
+		}
+		catch(ArithmeticException E) {
+			System.out.println("Exception occured...");
+		}
+		finally {
+			System.out.println("deleting the variables");
+			ans = 0;
+		}
+
+	}
+
+}
